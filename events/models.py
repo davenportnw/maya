622f2b16a97ae5d00feb2985c656a863ebeb1a94
nicole <nicole@nicoles-MacBook-Pro.local>
2023-11-12 12:14:29 -0800

    Create EventDetail class

--- events/models.py
+++ events/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 class Event(models.Model):
-    event_name = models.CharField(max_length=400)
-    timestamp =  models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=400)
     def __str__(self):
-        return f"{self.event_name} - {self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
+        return f"{self.name}"
+
+class EventDetail(models.Model):
+    timestamp =  models.DateTimeField(default=timezone.now)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return  f"{self.timestamp.strftime('%Y-%m-%d %H:%M:%S')}"
