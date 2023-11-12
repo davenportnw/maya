0b6e47acbbfa0f4ced1298b7fde9fdb18f47b22c
nicole <nicole@nicoles-MacBook-Pro.local>
2023-11-12 12:22:38 -0800

    Display date and time

--- events/views.py
+++ events/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from .models import Event
+from .models import Event, EventDetail
 
 
 # Create your views here.
 def index(request):
-    events = Event.objects.all()
-    return render(request, "landing/base.html",{'events': events})
+    events = Event.objects.prefetch_related('eventdetail_set').all()
+    return render(request, "landing/base.html", {'events': events})
 
