6b3fd474e9f67f75e8c5ed130046ca684d1336ab
nicole <nicole@nicoles-MBP.lan>
2023-11-14 17:21:37 -0800

    Able to existing event no matter how the event is capitalized.

--- events/views.py
+++ events/views.py
@@ -9,29 +9,37 @@ def index(request):
     return render(request, "landing/base.html", {'events': events})
 
 def add_event(request):
-    if request.method == "POST":
+    # if request.method == "POST":
         
-        # event_name = request.POST.get('event_name')
-        event_name = request.POST.get('event_name').strip().lower()    
+    #     # event_name = request.POST.get('event_name')
+    #     event_name = request.POST.get('event_name').strip().lower()    
+    #     if event_name:
+    #         # Check if an Event with this name already exists
+    #         event, created = Event.objects.get_or_create(name=event_name)
+
+    #         # Create a new EventDetail for this event
+    #         EventDetail.objects.create(event=event, timestamp=timezone.now())
+            
+    #         # Redirect to the list page or some confirmation page
+    #         return HttpResponseRedirect('/events/')  # Adjust the URL as needed
+    # # Redirect back to the form page if not a POST request or if the name is empty
+    # return HttpResponseRedirect('/events/')
+     if request.method == "POST":
+        event_name = request.POST.get('event_name').strip()
+
         if event_name:
-            # Check if an Event with this name already exists
-            event, created = Event.objects.get_or_create(name=event_name)
+            # Try to get an event with a case-insensitive match
+            event = Event.objects.filter(name__iexact=event_name).first()
+
+            # If event does not exist, create a new one
+            if not event:
+                event = Event.objects.create(name=event_name)
 
             # Create a new EventDetail for this event
             EventDetail.objects.create(event=event, timestamp=timezone.now())
-            
+
             # Redirect to the list page or some confirmation page
-            return HttpResponseRedirect('/events/')  # Adjust the URL as needed
-    # Redirect back to the form page if not a POST request or if the name is empty
-    return HttpResponseRedirect('/events/')
-    # if request.method == "POST":
-    #     event_name = request.POST.get('event_name')
-    #     print('test')
-    #     if event_name:
-    #         event = Event.objects.create(
-    #             name=event_name)
-    #         EventDetail.objects.create(
-    #             event=event, timestamp=timezone.now())
-    #     return HttpResponseRedirect('/events/')
+            return HttpResponseRedirect('/events/') 
+   
 
 
