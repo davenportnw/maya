26fb19a97859a46274308d81821daa8dc51d423c
nicole <nicole@nicoles-MBP.lan>
2023-11-14 16:50:34 -0800

    Add event from the frontend.

--- events/urls.py
+++ events/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'events'  # This is important for namespacing
 urlpatterns = [
     path('', views.index, name="index"),
-]
+    path('add_event', views.add_event, name='add_event'),
+
+]   
--- events/views.py
+++ events/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from .models import Event, EventDetail
-
+from django.utils import timezone
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     events = Event.objects.prefetch_related('eventdetail_set').all()
     return render(request, "landing/base.html", {'events': events})
 
+def add_event(request):
+    if request.method == "POST":
+        event_name = request.POST.get('event_name')
+        print('test')
+        if event_name:
+            event = Event.objects.create(
+                name=event_name)
+            EventDetail.objects.create(
+                event=event, timestamp=timezone.now())
+        return HttpResponseRedirect('/events/')
