75b0882d90873281e66c123a67c80201b898539b
Nicole Davenport <nicoledavenport@Nicoles-MBP.lan>
2023-11-24 00:46:22 -0600

    Add user as foreign key to events model

--- events/models.py
+++ events/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Event(models.Model):
     name = models.CharField(max_length=400)
     emoji = models.CharField(max_length=42, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     def __str__(self):
         return f"{self.name}"
 
