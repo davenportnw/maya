02563d75fad94889c6a6a26ade340a05df424498
Nicolie <nicolie@example.com>
2023-12-12 23:49:23 -0600

    Add optional timeof day to occurrence

--- events/models.py
+++ events/models.py
@@ -11,6 +11,7 @@ class Event(models.Model):
 
 class Occurrence(models.Model):
     timestamp =  models.DateField(default=timezone.now)
+    timeofday = models.DateTimeField(null=True)
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     def __str__(self):
         return  f"{self.timestamp.strftime('%Y-%m-%d')}"
